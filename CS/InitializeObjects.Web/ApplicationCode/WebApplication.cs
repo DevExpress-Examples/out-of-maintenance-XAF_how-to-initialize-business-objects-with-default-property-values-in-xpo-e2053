0f92abe9f70417727658a2e1275b1c9784e85f21
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/InitializeObjects.Web/ApplicationCode/WebApplication.cs
@@ -0,0 +1,66 @@
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+
+namespace InitializeObjects.Web {
+    public partial class InitializeObjectsAspNetApplication : WebApplication {
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProviderThreadSafe(args.ConnectionString, args.Connection);
+        }
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private InitializeObjects.Module.InitializeObjectsModule module3;        
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
+        private DevExpress.ExpressApp.Validation.ValidationModule module5;
+
+        public InitializeObjectsAspNetApplication() {
+            InitializeComponent();
+        }
+
+        private void InitializeObjectsAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+
+        protected override void OnLoggedOn(DevExpress.ExpressApp.LogonEventArgs e) {
+        base.OnLoggedOn(e);
+        ((ShowViewStrategy)base.ShowViewStrategy).CollectionsEditMode =
+            DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+        }
+
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new InitializeObjects.Module.InitializeObjectsModule();
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // module1
+            // 
+            this.module1.AdditionalExportedTypes.Add(typeof(DevExpress.Xpo.XPObjectType));
+            // 
+            // module5
+            // 
+            this.module5.AllowValidationDetailsAccess = true;
+            // 
+            // InitializeObjectsAspNetApplication
+            // 
+            this.ApplicationName = "InitializeObjects";
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.module3);            
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.securityModule1);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.InitializeObjectsAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
---
+++ VB/InitializeObjects.Module/Updater.vb
@@ -0,0 +1,42 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.ExpressApp
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace InitializeObjects.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As IObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+
+			Dim phone1 As PhoneNumber = ObjectSpace.FindObject(Of PhoneNumber)(CriteriaOperator.Parse("Number = '555-0101'"))
+			If phone1 Is Nothing Then
+				phone1 = ObjectSpace.CreateObject(Of PhoneNumber)()
+				phone1.Number = "555-0101"
+				phone1.PhoneType = "Home"
+				phone1.Save()
+			End If
+			Dim phone2 As PhoneNumber = ObjectSpace.FindObject(Of PhoneNumber)(CriteriaOperator.Parse("Number = '555-0102'"))
+			If phone2 Is Nothing Then
+				phone2 = ObjectSpace.CreateObject(Of PhoneNumber)()
+				phone2.Number = "555-0102"
+				phone2.PhoneType = "Work"
+				phone2.Save()
+			End If
+			Dim johnDoe As Contact = ObjectSpace.FindObject(Of Contact)(CriteriaOperator.Parse("FirstName = 'John' && LastName = 'Doe'"))
+			If johnDoe Is Nothing Then
+				johnDoe = ObjectSpace.CreateObject(Of Contact)()
+				johnDoe.FirstName = "John"
+				johnDoe.LastName = "Doe"
+				johnDoe.Save()
+			End If
+		End Sub
+	End Class
+End Namespace
